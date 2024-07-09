e6d0b9a0f9c196c1197302cf8e4e24b99be4e65b
vikki057 <165292890+vikki057@users.noreply.github.com>
2024-07-09 15:27:20 +0530

    Add files via upload

---
+++ primeno.cpp
@@ -0,0 +1,27 @@
+// Prime number:
+
+#include <iostream>
+using namespace std;
+
+// int main()
+// {
+//     int i, n, count = 0;
+//     cout << "enter a positive integer:";
+//     cin >> n;
+
+//     for (i = 1; i <= n; i++)
+//     {
+//         if (n % i == 0)
+//         {
+//             count++;
+//         }
+//     }
+//     if (count == 2)
+//     {
+//         cout << "it is a prime number";
+//     }
+//     else
+//     {
+//         cout << "it is not a prime number";
+//     }
+// }
