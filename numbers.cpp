6ae5c73192c00fdc5f2a125c25cc9531a70d842c
vikki057 <165292890+vikki057@users.noreply.github.com>
2024-07-10 20:40:32 +0530

    Add files via upload

---
+++ numbers.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int i,j,k,b,rows,stars;    
+//         cout<<"enter the number of rows you want:";
+//         cin>>rows;
+//         cout<<"enter b:";
+//         cin>>b;
+
+//         for(i=1;i<=rows;i++){
+//             for(k=1;k<=i;k++){
+//                 cout<<b;
+//                 b++;
+//             }
+//             cout<<endl;
+//         }
+// }
+
+
