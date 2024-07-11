66a1b1d65f0807e58d590650da0b40b031a62362
vikki057 <165292890+vikki057@users.noreply.github.com>
2024-07-11 23:49:32 +0530

    Add files via upload

---
+++ heart.cpp
@@ -0,0 +1,78 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a, row, col;
+//     a = 6;
+//     for (row = 0; row < a; row++)
+//     {
+//         for (col = 0; col < a + 1; col++)
+//         {
+//             if ((row == 0 && col % 3 != 0) || (row == 1 && col % 3 == 0) || (row - col == 2) || (row + col == 8))
+//             {
+//                 cout << "x";
+//             }
+//             else
+//             {
+
+//                 cout << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a, row, col;
+//     a = 9;
+//     for (row = 0; row < a-1; row++)
+//     {
+//         for (col = 0; col < a; col++)
+//         {
+//             if ((row == 0 && col % 4 != 0) || (row == 1 && col % 4 == 0) | (row == 2 && col % 4 == 0) || (row - col == 3) || (row + col == 11))
+//             {
+//                 cout << "*";
+//             }
+//             else
+//             {
+
+//                 cout << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a, row, col;
+//     a = 12;
+//     for (row = 0; row < a - 2; row++)
+//     {
+//         for (col = 0; col < a-1; col++)
+//         {
+//             if ((row == 0 && col % 5 != 0) || (row == 1 && col % 5 == 0) | (row == 2 && col % 5 == 0) || (row == 3 && col % 5 == 0) || (row - col == 4) || (row + col == 14))
+//             {
+//                 cout << ".";
+//             }
+//             else
+//             {
+
+//                 cout << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
