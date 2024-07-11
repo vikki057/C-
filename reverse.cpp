bb4f3f75c65dbb77219cdbc67c78ba58ae191e71
vikki057 <165292890+vikki057@users.noreply.github.com>
2024-07-11 20:55:19 +0530

    Add files via upload

---
+++ reverse.cpp
@@ -0,0 +1,15 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int a , b, rev=0;
+//     cout<<"Enter the number:";
+//     cin>>a;
+
+//     while(a>0){
+//         b=a%10;
+//         rev=rev*10+b;
+//         a=a/10;
+//     }
+//     cout<<rev;
+// }
