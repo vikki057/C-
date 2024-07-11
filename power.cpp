2a3bf02730bbdfdab348a138fdfe5b0337ab1155
vikki057 <165292890+vikki057@users.noreply.github.com>
2024-07-11 23:03:36 +0530

    Add files via upload

---
+++ power.cpp
@@ -0,0 +1,56 @@
+// Power of a number:
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// int main(){
+//     int a,b,power;
+//     cout<<"Enter first number:";
+//     cin>>a;
+//     cout<<"Enter second number:";
+//     cin>>b;
+//     power=pow(a,b);
+//     cout<<power;
+// }
+
+// Power of a number using function:
+
+// #include<iostream>
+// using namespace std;
+
+// int power(int a, int b){
+//     int pow=1;
+//     for(int i=1; i<=b; i++){
+//         pow=pow*a;
+//     }
+//     return pow;
+// }
+
+// int main(){
+//     int a,b,c;
+//     cout<<"enter the number:";
+//     cin>>a;
+//     cout<<"enter the exponent:";
+//     cin>>b;
+//     c=power(a,b);
+//     cout<<c;
+// }
+
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+
+// int powerof(int a , int b){
+//     return pow(a,b);
+// }
+
+// int main(){
+//     int a,b,power;
+//     cout<<"enter the value of a :";
+//     cin>>a;
+//     cout<<"enter the value of b :";
+//     cin>>b;
+//     power=powerof(a,b);
+//     cout<<power;
+// }
