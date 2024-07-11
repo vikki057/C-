6a0e8e5e649b2ec72aac30dbd91c4ae19987f5f4
vikki057 <165292890+vikki057@users.noreply.github.com>
2024-07-11 22:35:07 +0530

    Add files via upload

---
+++ lyfunction.cpp
@@ -0,0 +1,36 @@
+// #include <iostream>
+// using namespace std;
+
+// int leapyear(int a)
+// {
+//     if (a % 4 == 0)
+//     {
+//         if (a % 100 == 0)
+//         {
+//             if (a % 400 == 0)
+//             {
+//                 return true;
+//             }
+//             else
+//             {
+//                 return false;
+//             }
+//             return true;
+//         }
+//         else
+//         {
+//             return false;
+//         }
+//     }
+// }
+// int main(){
+//     int year;
+//     cout<<"enter the year:";
+//     cin>>year;
+//     if(leapyear(year)){
+//         cout<<"the "<<year<<" is leap year";
+//     }
+//     else{
+//         cout<<"the "<<year<<" is not leap year";
+//     }
+// }
