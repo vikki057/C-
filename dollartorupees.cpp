eef3aaebe9bd2334aa801cd8f6fc75f22831ceb1
Vivek Gururani <165292890+vikki057@users.noreply.github.com>
2024-07-13 23:37:24 +0530

    Add files via upload

---
+++ dollartorupees.cpp
@@ -0,0 +1,31 @@
+// simple:
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     float rup,dol;
+//     cout<<"Enter the amount in dollars:";
+//     cin>>dol;
+//     rup=dol*83.52;
+//     cout<<"1 dollar is :"<< rup << " rupees";
+// }
+
+
+// using function:
+
+// #include<iostream>
+// using namespace std;
+
+// float rupees(float a){
+//     return a;
+// }
+
+// int main(){
+//     float dol,rup,result;
+//     cout<<"Enter the amount in dollars:";
+//     cin>>dol;
+//     rup = dol*83.52;
+//     result = rupees(rup);
+//     cout<< dol << " dollars in rupees is : " << result << " rupees ";
+// }
