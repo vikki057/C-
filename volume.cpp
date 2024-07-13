71f2397a493a5bdce210066548053930a448b0eb
Vivek Gururani <165292890+vikki057@users.noreply.github.com>
2024-07-13 23:11:33 +0530

    Add files via upload

---
+++ volume.cpp
@@ -0,0 +1,20 @@
+// #include <iostream>
+// using namespace std;
+
+// int volume(float a, float b, float c)
+// {
+//     return a * b * c;
+// }
+
+// int main()
+// {
+//     float a, b, c, vol;
+//     cout << "Enter the value of a:";
+//     cin >> a;
+//     cout << "Enter the value of b:";
+//     cin >> b;
+//     cout << "Enter the value of c:";
+//     cin >> c;
+//     vol = volume(a, b, c);
+//     cout << "The volume of cube is:" << vol;
+// }
