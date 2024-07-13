45fe45258c3b7c52c238399ae366f615b75ce86b
Vivek Gururani <165292890+vikki057@users.noreply.github.com>
2024-07-13 23:26:09 +0530

    Add files via upload

---
+++ palendrome.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a, b, num = 0;
+//     cout << "Enter the number:";
+//     cin >> a;
+//     int n = a;
+//     while (a > 0)
+//     {
+//         b = a % 10;
+//         num = num * 10 + b;
+//         a = a / 10;
+//     }
+//     if (n == num)
+//     {
+//         cout << "Palendrome number";
+//     }
+//     else
+//     {
+//         cout << "Not a Palendrome number";
+//     }
+// }
