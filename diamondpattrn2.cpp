b49c4acd99dfe27f7212e78ce91b416a07ab4440
Vivek Gururani <165292890+vikki057@users.noreply.github.com>
2024-07-14 22:28:09 +0530

    Add files via upload

---
+++ diamondpattrn2.cpp
@@ -0,0 +1,30 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int i,j,k,row;
+//     char A=65;
+//     cout<<"Enter the no of rows you want:";
+//     cin>>row;
+
+//     for(i=1;i<=row;i++){
+//         for(j=1;j<=(row-i);j++){
+//             cout<<" ";
+//         }
+//         for(k=1;k<=2*i-1;k++){
+//             cout<<A;
+//             A++;
+//         }
+//         cout<<endl;
+//     }
+//         for(i=row;i>=1;i--){
+//         for(j=row-i;j>=1;j--){
+//             cout<<" ";
+//         }
+//         for(k=2*i-1;k>=1;k--){
+//             cout<<A;
+//             A++;
+//         }
+//         cout<<endl;
+//     }
+// }
