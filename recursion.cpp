b468770f18cb5a6f6a3f01ceb93f3d3f32741875
Vivek Gururani <165292890+vikki057@users.noreply.github.com>
2024-07-16 22:15:02 +0530

    Add files via upload

---
+++ recursion.cpp
@@ -0,0 +1,33 @@
+// #include <iostream>
+// using namespace std;
+
+// int fibonacci(int a)
+// {
+//     if (a == 0)
+//     {
+//         return 0;
+//     }
+
+//     else if (a == 1)
+//     {
+//         return 1;
+//     }
+
+//     else
+//     {
+//         return (fibonacci(a - 1) + fibonacci(a - 2));
+//     }
+// }
+
+// int main()
+// {
+//     int a;
+//     cout << "Enter the no of elements you want: ";
+//     cin >> a;
+
+//     cout << "Fibonacci series : ";
+//     for (int i = 0; i <= a; i++)
+//     {
+//         cout << " " << fibonacci(i);
+//     }
+// }
