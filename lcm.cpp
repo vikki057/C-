38f5847514b64bf9e19fa36fdce63f0cc2f82d56
Vivek Gururani <165292890+vikki057@users.noreply.github.com>
2024-07-17 22:52:05 +0530

    Add files via upload

---
+++ lcm.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a, b, max;
+//     cout << "Enter first number:";
+//     cin >> a;
+//     cout << "Enter second number:";
+//     cin >> b;
+
+//     max = (a > b) ? a : b;
+//     do
+//     {
+//         if (max % a == 0 && max % b == 0)
+//         {
+//             cout << "LCM :" << max;
+//             break;
+//         }
+//         else
+//         {
+//             max++;
+//         }
+//     } while (true);
+//     return 0;
+// }
+
