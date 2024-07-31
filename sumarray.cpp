976192bb54f16cbccf75b21a2c8b7f2386e052e8
Vivek Gururani <165292890+vikki057@users.noreply.github.com>
2024-08-01 00:05:27 +0530

    Add files via upload

---
+++ sumarray.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a[2][2], b[2][2], c[2][2];
+//     int i, j;
+//     cout << "Enter the elements of array 1:" << endl;
+//     for (i = 0; i <= 1; i++)
+//     {
+//         for (j = 0; j <= i; j++)
+//         {
+//             cin >> a[i][j];
+//         }
+//     }
+//     cout << "Enter the elements of array 2:" << endl;
+//     for (i = 0; i <= 1; i++)
+//     {
+//         for (j = 0; j <= i; j++)
+//         {
+//             cin >> b[i][j];
+//         }
+//     }
+//     cout<<"The array is: ";
+//     for(i=0;i<=1;i++){
+//         for(j=0;j<=i;j++){
+//             c[i][j] = a[i][j] + b[i][j];
+//             cout<<c[i][j];
+//         }
+//     }
+// }
